81bf9f4b75ff433747e6bfd66701280262654a93
Michael Chiew <41043702+michaelchiew08@users.noreply.github.com>
2023-01-26 11:33:51 +0800

    Allow new annotation and nonnull

--- android/src/main/java/com/uupy/flutter_pda_scanner/PdaScannerPlugin.java
+++ android/src/main/java/com/uupy/flutter_pda_scanner/PdaScannerPlugin.java
@@ -1,11 +1,14 @@
 package com.uupy.flutter_pda_scanner;
 
-import android.content.IntentFilter;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
