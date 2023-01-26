4f0bebdf60d459a6705ef518bb348e373de2e73c
Michael Chiew <41043702+michaelchiew08@users.noreply.github.com>
2023-01-26 13:25:04 +0800

    Temporary revert to V1

--- example/android/app/src/main/java/com/shinow/pda_scanner_example/MainActivity.java
+++ example/android/app/src/main/java/com/shinow/pda_scanner_example/MainActivity.java
@@ -1,6 +1,8 @@
 package com.uupy.flutter_pda_scanner_example;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
